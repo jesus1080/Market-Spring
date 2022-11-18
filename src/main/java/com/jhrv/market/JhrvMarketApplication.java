af6e8c987ffd417017fdb4bcf3e4eeaf46878993
Jesus Ruiz <jesusruiz@unicauca.edu.co>
2022-11-17 20:17:11 -0500

    init commit

---
+++ src/main/java/com/jhrv/market/JhrvMarketApplication.java
@@ -0,0 +1,13 @@
+package com.jhrv.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JhrvMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JhrvMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhrv/market/JhrvMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhrv.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JhrvMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
